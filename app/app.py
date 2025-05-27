f2f2c778492ed33ab2c54b7d5b5e5512a62060e6
teste <teste@teste.com>
2025-05-27 17:50:27 -0300

    teste

--- app/app.py
+++ app/app.py
@@ -3,5 +3,5 @@ import os
 
 site = os.getenv("INPUT_SITE")
 resposta = requests.get(site)
-os.system("echo STATUS_CODE={resposta.status_code} >> $GITHUB_OUTPUT")
+os.system(f"echo STATUS_CODE={resposta.status_code} >> $GITHUB_OUTPUT")
 
